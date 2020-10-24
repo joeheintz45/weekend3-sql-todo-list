fb59dfdd3e28e4cc10ee370d30cc0662b40dbe47
Joey Heintz <joeheintz45@gmail.com>
2020-10-24 15:16:11 -0500

    added the basic html and js for picking up inputs and the get and post route for displaying data and addind data

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,9 @@ $(document).ready(onReady);
 function onReady() {
   console.log('Ready');
   $('.js-add-task').on('click', makeTask);
+  $('.js-task-table').on('click', '.js-delete', deleteTask);
+
+  getTask();
 }
 
 function makeTask() {
@@ -19,6 +22,21 @@ function clearInput() {
   $('.js-new-task').val('');
 }
 
+function render(tasks) {
+  $('.js-task-list').empty();
+  for (let i = 0; i < tasks.length; i++) {
+    const task = tasks[i];
+
+    $('.js-task-list').append(`
+      <tr>
+        <td>${task.new_task}</td>
+        <td>${task.completed}</td>
+        <td><button class="js-delete" data-id="${task.id}">Delete</button></td>
+      </tr>
+    `);
+  }
+}
+
 function getTask() {
   $.ajax({
     type: 'GET',
@@ -27,6 +45,35 @@ function getTask() {
     .then((response) => {
       console.log(response);
       render(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function postTask(newTask) {
+  $.ajax({
+    type: 'POST',
+    url: '/tasks',
+    data: newTask,
+  })
+    .then((response) => {
+      getTask();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function deleteTask() {
+  const taskId = $(this).data('id');
+
+  $.ajax({
+    type: 'DELETE',
+    url: `/tasks/${taskId}`,
+  })
+    .then((response) => {
+      getTask();
     })
     .catch((err) => {
       console.log(err);
