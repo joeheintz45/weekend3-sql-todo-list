b0033029af521513c2063f967a2a7ac41e0e79f8
Joey Heintz <joeheintz45@gmail.com>
2020-10-24 01:43:27 -0500

    prepped the client side files with basic checks for file validation

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,7 +1,7 @@
 const pg = require('pg');
 
 const Pool = pg.Pool;
-const pool = new POOL({
+const pool = new Pool({
   database: 'weekend-to-do-app',
   host: 'localhost',
   port: 5432,
--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -2,3 +2,5 @@ const express = require('express');
 const router = express.Router();
 
 const pool = require('../modules/pool');
+
+module.exports = router;
